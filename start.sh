b3f6310e80ae0e437db587b19699752495e7bb55
cftgvthyhyhy <113160677+cftgvthyhyhy@users.noreply.github.com>
2022-09-09 11:01:49 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/EvamariaTG/EvaMaria.git /EvaMaria
+  git clone https://github.com/cftgvthyhyhy/SLFilmsBot.git /SLFilmsBot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /EvaMaria
+  git clone $UPSTREAM_REPO /SLFilmsBot
 fi
-cd /EvaMaria
+cd /SLFilmsBot
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
